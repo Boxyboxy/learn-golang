dd1286acd383c97b2dd91b7c9008fa7b714edcb6
boxyboxy <eugenengboxiang@gmail.com>
2024-06-11 15:15:05 +0800

    Decided to not start with GOPL, switched to learn go with tests track. Created first hello_test

---
+++ helloworld/hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want) // %q prints the value as double-quoted string
+	}
+}
+
+// go mod init hello
+// create go.mod file
+// This file tells go tools essential information about my code
+// writing tests:
+/*
+1.file name: xxx_test.go
+2. test function must start with the word Test
+3. test function takes one argument only t *testing.T
+4. Need to import "testing"
+*/
