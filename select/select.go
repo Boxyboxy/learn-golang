dc70057bd1acb230697fb28081bbe6daef5cc616
boxyboxy <eugenengboxiang@gmail.com>
2024-06-20 19:25:17 +0800

    Select: - helps you wait on multiple channels - Sometimes you will want to include time.After in one of your cases to prevent your system blocking forever httptest: - Convenient way of creating test servers so you can have reliable and controllable tests - Uses the same interfaces as the real net/http servers which is consistent and less for you to learn

---
+++ select/select.go
@@ -0,0 +1,54 @@
+package sel
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+/*
+	func Racer(a, b string) (winner string) {
+		aDuration := measureReponseTime(a)
+
+		bDuration := measureReponseTime(b)
+
+		if aDuration < bDuration {
+			return a
+		}
+
+		return b
+	}
+
+	func measureReponseTime(url string) time.Duration {
+		start := time.Now()
+		http.Get(url)
+		return time.Since(start)
+	}
+*/
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	// no received values, the following syntax just waits for the channels to be closed
+	// select is just used to determine which requests finishes first
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+// In the following code, nothing is being communicated through the channel
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
